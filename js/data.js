e7fe2340bbc9c60127a4a176f069a860deb75d30
tumanob <tumanob@gmail.com>
2015-07-17 23:35:54 +0600

    new cat for numbers and tunes for feature one page cards

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var number;
+ number = 1; // start global declaration for numbers   - start from 1
+var maxslides = 29;  // maimum number of words to show
+
+numarray = new Array(
+{"main":"29","img":"0","mtext":"ноль"},
+{"main":"бир","img":"1","mtext":"Один<br/>One"},
+{"main":"эки","img":"2","mtext":"Два<br/>Two"},
+{"main":"үч","img":"3","mtext":"Три<br/>Three"},
+{"main":"төрт","img":"4","mtext":"Четыре<br/>Four"},
+{"main":"беш","img":"5","mtext":"Пять<br/>Five"},
+{"main":"алты","img":"6","mtext":"Шесть<br/>Six"},
+{"main":"жети","img":"7","mtext":"Семь<br/>Seven"},
+{"main":"сегиз","img":"8","mtext":"Восемь<br/>Eight"},
+{"main":"тогуз","img":"9","mtext":"Девять<br/>Nine"},
+{"main":"он","img":"10","mtext":"Десять<br/>Ten"},
+{"main":"он бир","img":"11","mtext":"Одинадцать<br/>Eleven"},
+{"main":"он эки","img":"12","mtext":"Двенадцать<br/>Twelve"},
+{"main":"он үч","img":"13","mtext":"Тринадцать<br/>Therteen"},
+{"main":"он төрт","img":"14","mtext":"Четырнадцать<br/>Fourteen"},
+{"main":"он беш","img":"15","mtext":"Пятнадцать<br/>Fifteen"},
+{"main":"он алты","img":"16","mtext":"Шестнадцать<br/>Sixteen"},
+{"main":"он жети","img":"17","mtext":"Семьнадцать<br/>Seventeen"},
+{"main":"он сегиз","img":"18","mtext":"Восемьнадцать<br/>Eighteen"},
+{"main":"он тогуз","img":"19","mtext":"Девятьнадцать<br/>Nineteen"},
+{"main":"жыйырма","img":"20","mtext":"Двадцать<br/>Twenty"},
+{"main":"отуз","img":"30","mtext":"Тридцать<br/>Thirty"},
+{"main":"кырк","img":"40","mtext":"Сорок<br/>Forty"},
+{"main":"элүү","img":"50","mtext":"Пятьдесят<br/>Fifty"},
+{"main":"алтымыш","img":"60","mtext":"Шестьдесят<br/>Syxty"},
+{"main":"жетимиш","img":"70","mtext":"Семьдесят<br/>Seventy"},
+{"main":"сексен","img":"80","mtext":"Восемьдесят<br/>Eighty"},
+{"main":"токсон","img":"90","mtext":"Девяносто<br/>Ninety"},
+{"main":"жүз","img":"100","mtext":"Сто<br/>Hundred"},
+{"main":"миң","img":"1000","mtext":"Тысяча<br/>Thousand"}
+
+);
--- js/my.js
+++ js/my.js
@@ -1,75 +1,78 @@
 
 
- 
+
 // Put your custom code here
 $('document').ready(function () {
 
-  
-  
+  var hash = window.location.hash.substring(1);
+  var nText = [];
+  nText = numarray;
+  //alert (hash);
+
 	//  run on page start to pronounce first word
- 
+
 	 $(document).on('swipeleft swiperight',function(event, data){
 	 	      	event.stopImmediatePropagation();
 				if(event.type =="swipeleft"){
-					 
+
 					 nextClick();
 				}
 				else if(event.type =="swiperight")
 				{
 					  prevClick();
 				}
-	 	       
+
 	 	});
-		
-		 
-    $('#play').click(function(){  
+
+
+    $('#play').click(function(){
 			showTranslationAndSoundPlay();
-			
-	  });   
-	  
-      $('#next').click(function(){  
+
+	  });
+
+      $('#next').click(function(){
 		  nextClick();
-			
-  	  });  
-	  
-      $('#prev').click(function(){ 
+
+  	  });
+
+      $('#prev').click(function(){
 		  prevClick();
-  	  });   
-	  
-	 
-	   
-	   
-	  var ChangeImage = function($param) {   $('#numberimage').attr("src", 'assets/'+nText[$param].img+'.jpg');   }; 
-	  var ChangeText = function($param) {  
-		  $('#mainnum').replaceWith('<h3 id="mainnum">'+nText[$param].main+'</h3>'); 
-		  $('#text').replaceWith('<h4 id="text">'+nText[$param].mtext+'</h4>'); 
-     	}; 
-	  
-	  var nextClick = function() {  
+  	  });
+
+
+
+
+	  var ChangeImage = function($param) {   $('#numberimage').attr("src", 'assets/'+nText[$param].img+'.jpg');   };
+	  var ChangeText = function($param) {
+		  $('#mainnum').replaceWith('<h3 id="mainnum">'+nText[$param].main+'</h3>');
+		  $('#text').replaceWith('<h4 id="text">'+nText[$param].mtext+'</h4>');
+     	};
+
+	  var nextClick = function() {
 		  if(number==maxslides){number=0}
 	  	  number=number+1;
-  			
+
 			if(number<=maxslides && number>0){
 				ChangeImage(number);
-				ChangeText(number);	
-				hideTranslation();	
-				//PlaySound(number);	
+				ChangeText(number);
+				hideTranslation();
+				//PlaySound(number);
 			}
-			else 
+			else
 			{
 				number=number-1;
 			}
-		}; 
-		
-  	 
-		
-	  var prevClick = function() { 
-		  if(number==1){number=maxslides+1} 
+		};
+
+
+
+	  var prevClick = function() {
+		  if(number==1){number=maxslides+1}
 		  number=number-1;
-		  
+
   			if(number<=maxslides && number>0){
 				ChangeImage(number);
-				ChangeText(number);	
+				ChangeText(number);
 				hideTranslation();
 				//PlaySound(number);
 			}
@@ -77,30 +80,30 @@ $('document').ready(function () {
 			{
 				number=number+1;
 			}
-	  
-	  	}; 
-		
-    	  var showTranslationAndSoundPlay = function() {  
-  		  
+
+	  	};
+
+    	  var showTranslationAndSoundPlay = function() {
+
 		   $('#mainnum').css("visibility", "visible");
 		   PlaySound(number);
-		   
-    		}; 
-		
-      	  var hideTranslation = function() {  
-  		  		
-				$('#mainnum').css("visibility", "hidden");
-				
-      		}; 
-			
-		
-		
-  	  var PlaySound = function($param) { 
 
-	  	 $.playSounds('assets/'+nText[$param].img+'.mp3');   
-							  					   
-  	   }; 
-	   
+    		};
+
+      	  var hideTranslation = function() {
+
+				$('#mainnum').css("visibility", "hidden");
+
+      		};
+
+
+
+  	  var PlaySound = function($param) {
+        //alert("aaa");
+	  	 $.playSounds('assets/'+nText[$param].img+'.mp3');
+       //alert(nText[$param].img);
+  	   };
+
 	   $(document).keydown(function(e) {
 	       switch(e.which) {
 	           case 37: // left
@@ -123,39 +126,40 @@ $('document').ready(function () {
 	       }
 	       e.preventDefault(); // prevent the default action (scroll / move caret)
 	   });
-	   
-	   
+
+
  });
- 
- 
+
+
  /* -----------  --------------- */
 
  function exitFromApp()
         {
              navigator.app.exitApp();
         }
-		
+
 
  (function($){
 
    $.extend({
      playSounds: function(){
-       return $("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false' class='playSound'>").appendTo('body');
+      // alert("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false' class='playSound'>");
+       $('#sound1').attr("src", arguments[0]); // = arguments[0];
+       document.getElementById('sound1').play();
+      // return $("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false' class='playSound'>").appendTo('body');
      }
    });
 
  })(jQuery);
- 
- 
+
+
 
      function onDeviceReady() {
        document.removeEventListener('deviceready', onDeviceReady, false);
-       
+
 
        // display a banner at startup
-     
+
      }
 
      document.addEventListener("deviceready", onDeviceReady, false);
-	 
-	 
