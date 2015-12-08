8b87b3131ca5f70e85e895d23e34a32fcdfb9af6
tumanob <tumanob@gmail.com>
2015-12-08 11:44:39 +0600

    adding bootstrap - updating jquery  - updating main theme

--- js/my.js
+++ js/my.js
@@ -1,75 +1,96 @@
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+
+    });
+
+   // Remove menu for searching
+   $('#search-trigger').click(function () {
+        $('.navbar-nav').removeClass('slide-in');
+        $('.side-body').removeClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').removeClass('slide-in');
+
+    });
+});
 
 
- 
+
 // Put your custom code here
 $('document').ready(function () {
 
-  
-  
+
+
 	//  run on page start to pronounce first word
- 
+
 	 $(document).on('swipeleft swiperight',function(event, data){
 	 	      	event.stopImmediatePropagation();
 				if(event.type =="swipeleft"){
-					 
+
 					 nextClick();
 				}
 				else if(event.type =="swiperight")
 				{
 					  prevClick();
 				}
-	 	       
+
 	 	});
-		
-		 
-    $('#play').click(function(){  
+
+
+    $('#play').click(function(){
 			showTranslationAndSoundPlay();
-			
-	  });   
-	  
-      $('#next').click(function(){  
+
+	  });
+
+      $('#next').click(function(){
 		  nextClick();
-			
-  	  });  
-	  
-      $('#prev').click(function(){ 
+
+  	  });
+
+      $('#prev').click(function(){
 		  prevClick();
-  	  });   
-	  
-	 
-	   
-	   
-	  var ChangeImage = function($param) {   $('#numberimage').attr("src", 'assets/'+nText[$param].img+'.jpg');   }; 
-	  var ChangeText = function($param) {  
-		  $('#mainnum').replaceWith('<h3 id="mainnum">'+nText[$param].main+'</h3>'); 
-		  $('#text').replaceWith('<h4 id="text">'+nText[$param].mtext+'</h4>'); 
-     	}; 
-	  
-	  var nextClick = function() {  
+  	  });
+
+
+
+
+	  var ChangeImage = function($param) {   $('#numberimage').attr("src", 'assets/'+nText[$param].img+'.jpg');   };
+	  var ChangeText = function($param) {
+		  $('#mainnum').replaceWith('<h3 id="mainnum">'+nText[$param].main+'</h3>');
+		  $('#text').replaceWith('<h4 id="text">'+nText[$param].mtext+'</h4>');
+     	};
+
+	  var nextClick = function() {
 		  if(number==maxslides){number=0}
 	  	  number=number+1;
-  			
+
 			if(number<=maxslides && number>0){
 				ChangeImage(number);
-				ChangeText(number);	
-				hideTranslation();	
-				//PlaySound(number);	
+				ChangeText(number);
+				hideTranslation();
+				//PlaySound(number);
 			}
-			else 
+			else
 			{
 				number=number-1;
 			}
-		}; 
-		
-  	 
-		
-	  var prevClick = function() { 
-		  if(number==1){number=maxslides+1} 
+		};
+
+
+
+	  var prevClick = function() {
+		  if(number==1){number=maxslides+1}
 		  number=number-1;
-		  
+
   			if(number<=maxslides && number>0){
 				ChangeImage(number);
-				ChangeText(number);	
+				ChangeText(number);
 				hideTranslation();
 				//PlaySound(number);
 			}
@@ -77,30 +98,30 @@ $('document').ready(function () {
 			{
 				number=number+1;
 			}
-	  
-	  	}; 
-		
-    	  var showTranslationAndSoundPlay = function() {  
-  		  
+
+	  	};
+
+    	  var showTranslationAndSoundPlay = function() {
+
 		   $('#mainnum').css("visibility", "visible");
 		   PlaySound(number);
-		   
-    		}; 
-		
-      	  var hideTranslation = function() {  
-  		  		
-				$('#mainnum').css("visibility", "hidden");
-				
-      		}; 
-			
-		
-		
-  	  var PlaySound = function($param) { 
 
-	  	 $.playSounds('assets/'+nText[$param].img+'.mp3');   
-							  					   
-  	   }; 
-	   
+    		};
+
+      	  var hideTranslation = function() {
+
+				$('#mainnum').css("visibility", "hidden");
+
+      		};
+
+
+
+  	  var PlaySound = function($param) {
+
+	  	 $.playSounds('assets/'+nText[$param].img+'.mp3');
+
+  	   };
+
 	   $(document).keydown(function(e) {
 	       switch(e.which) {
 	           case 37: // left
@@ -123,18 +144,18 @@ $('document').ready(function () {
 	       }
 	       e.preventDefault(); // prevent the default action (scroll / move caret)
 	   });
-	   
-	   
+
+
  });
- 
- 
+
+
  /* -----------  --------------- */
 
  function exitFromApp()
         {
              navigator.app.exitApp();
         }
-		
+
 
  (function($){
 
@@ -145,17 +166,15 @@ $('document').ready(function () {
    });
 
  })(jQuery);
- 
- 
+
+
 
      function onDeviceReady() {
        document.removeEventListener('deviceready', onDeviceReady, false);
-       
+
 
        // display a banner at startup
-     
+
      }
 
      document.addEventListener("deviceready", onDeviceReady, false);
-	 
-	 
